539f7e99a9401b76556131294bb6940c88708951
himesh-cm <106069365+himesh-cm@users.noreply.github.com>
2022-06-07 16:55:04 +0530

    testing widgets

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      _counter = _counter + 2;
+      _counter = _counter + 1;
     });
   }
 
